9ef6b3a61d9e7b3ae4af2027aed7a65cb9dfeb4f
IT21169380 <isurusanka98@gmail.com>
2023-05-27 20:42:13 +0530

    nava bar created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Isuru Madusanka</h1>
+    <div>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg sticky-top">
+        <div className="container-fluid">
+          <span className="fw-bold display-6 m-2">Isuru Masuasnka</span>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
